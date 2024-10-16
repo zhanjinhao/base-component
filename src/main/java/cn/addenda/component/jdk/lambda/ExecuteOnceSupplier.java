7151c579d02b01f8880975ca6122f7f977f95523
addenda <isjinhao@163.com>
2024-10-16 18:04:58 +0800

    feature: ExecuteOnce相关

---
+++ src/main/java/cn/addenda/component/jdk/lambda/ExecuteOnceSupplier.java
@@ -0,0 +1,22 @@
+package cn.addenda.component.jdk.lambda;
+
+import java.util.function.Supplier;
+
+public class ExecuteOnceSupplier<R> implements Supplier<R> {
+  private final Supplier<R> supplier;
+  private R r;
+
+  public ExecuteOnceSupplier(Supplier<R> supplier) {
+    this.supplier = supplier;
+  }
+
+  @Override
+  public R get() {
+    return r == null ? (r = supplier.get()) : r;
+  }
+
+  public static <R> ExecuteOnceSupplier<R> of(Supplier<R> supplier) {
+    return new ExecuteOnceSupplier<>(supplier);
+  }
+
+}
