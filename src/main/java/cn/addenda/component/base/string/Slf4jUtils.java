c6ef23c02a21038e23b32ad39048733362d0179e
addenda <isjinhao@163.com>
2025-06-17 15:27:42 +0800

    feature：Slf4jUtils.java add todo

--- src/main/java/cn/addenda/component/base/string/Slf4jUtils.java
+++ src/main/java/cn/addenda/component/base/string/Slf4jUtils.java
@@ -8,6 +8,7 @@ import org.slf4j.helpers.MessageFormatter;
 public class Slf4jUtils {
 
   public static String format(String tql, Object... args) {
+    // todo 检测是否存在MessageFormatter，不存在使用StrFormatUtils
     return MessageFormatter.arrayFormat(tql, args).getMessage();
   }
 
